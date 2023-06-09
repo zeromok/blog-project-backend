57183f1a4e8ec7ed84514c5607e255879a3026d3
Daniel <byoung.mokk@gmail.com>
2023-06-09 15:34:45 +0900

    project init

---
+++ src/main/java/com/example/blogprojectbackend/BlogProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogProjectBackendApplication.class, args);
+    }
+
+}
