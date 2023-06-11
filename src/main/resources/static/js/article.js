54bce88c4bf0aec7524239c97e81667b8a945e64
Daniel <byoung.mokk@gmail.com>
2023-06-11 09:06:10 +0900

    Feat: 수정 기능 구현하기

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,5 +1,6 @@
-const deleteButton = document.getElementById('delete-btn');
 
+// * 삭제
+const deleteButton = document.getElementById('delete-btn');
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('article-id').value;
@@ -11,4 +12,28 @@ if (deleteButton) {
                 location.replace('/articles');
             });
     });
+}
+
+// * 수정
+const modifyButton = document.getElementById('modify-btn');
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value,
+            })
+        })
+            .then(() => {
+                alert('수정이 완료되었습니다.');
+                location.replace('/articles');
+            })
+    })
 }
