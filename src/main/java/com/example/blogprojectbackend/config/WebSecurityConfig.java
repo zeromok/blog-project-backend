44bab403a0bd68260770eacd6200daf54430c666
Daniel <byoung.mokk@gmail.com>
2023-06-12 00:46:02 +0900

    Feat: 시큐리티 설정

---
+++ src/main/java/com/example/blogprojectbackend/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.blogprojectbackend.config;
+
+import com.example.blogprojectbackend.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private UserDetailService userDetailService;
+
+    @Bean
+    public WebSecurityCustomizer configure() { // 시큐리티 설정 사용자 정의화
+
+        return (web) -> web.ignoring() // 시큐리티의 인증, 인가 모든 곳에 적용하지 않음
+                .requestMatchers(toH2Console()) // H2 DB 관련
+                .requestMatchers("/static/**"); // 정적메서드
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception { // HTTP 요청에 대한 시큐리티 설정
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request
+                            .requestMatchers(toH2Console()).permitAll()
+                            .requestMatchers("/login", "/signup", "/user").permitAll();
+                })
+                .formLogin(login -> {
+                    login
+                            .loginPage("/login")
+                            .defaultSuccessUrl("/article");
+                })
+                .logout(logout -> {
+                    logout
+                            .logoutUrl("/logout")
+                            .logoutSuccessUrl("/login")
+                            .invalidateHttpSession(true); // 로그아웃 시 세션 무효
+                });
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() { // 패스워드 인코더
+        return new BCryptPasswordEncoder();
+    }
+
+}
