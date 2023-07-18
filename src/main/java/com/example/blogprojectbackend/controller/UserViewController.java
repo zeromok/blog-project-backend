e61fcfcaa9ac6416ac6467c505c8899f23c56c04
Daniel <byoung.mokk@gmail.com>
2023-07-18 15:50:58 +0900

    Refactor: 루트페이지 진입 시 /login Redirect

--- src/main/java/com/example/blogprojectbackend/controller/UserViewController.java
+++ src/main/java/com/example/blogprojectbackend/controller/UserViewController.java
@@ -1,10 +1,17 @@
 package com.example.blogprojectbackend.controller;
 
+import org.apache.catalina.WebResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class UserViewController {
+
+    @GetMapping( "/" )
+    public String  init( ) {
+        return "redirect:login";
+    }
 
     @GetMapping("/login")
     public String login() {
