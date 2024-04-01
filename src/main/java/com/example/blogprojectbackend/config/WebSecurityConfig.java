fcd97fec2c78ed207031cbd85fd7350deef402d6
Daniel <byoung.mokk@gmail.com>
2024-04-01 18:02:09 +0900

    Fix: Security Provider 비활성화

--- src/main/java/com/example/blogprojectbackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/blogprojectbackend/config/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package com.example.blogprojectbackend.config;
 
-import com.example.blogprojectbackend.service.UserDetailService;
-import lombok.RequiredArgsConstructor;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import com.example.blogprojectbackend.service.UserDetailService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
