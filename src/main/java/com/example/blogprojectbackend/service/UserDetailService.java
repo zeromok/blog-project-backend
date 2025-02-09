784f242156636a6a010e7c7552e8772b371cbdf7
Daniel <byoung.mokk@gmail.com>
2025-02-09 13:01:52 +0900

    🩹Fix: 주석제거 및 변수명 수정

--- src/main/java/com/example/blogprojectbackend/service/UserDetailService.java
+++ src/main/java/com/example/blogprojectbackend/service/UserDetailService.java
@@ -11,14 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
-    // UserDetailsService: 스프링 스큐리티에서 사용자의 정보를 가져오는 인터페이스
 
     private final UserRepository userRepository;
 
     @Override
     public User loadUserByUsername(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            .orElseThrow(() -> new UsernameNotFoundException(""));
     }
 
 }
--- src/main/java/com/example/blogprojectbackend/service/UserService.java
+++ src/main/java/com/example/blogprojectbackend/service/UserService.java
@@ -1,23 +1,25 @@
 package com.example.blogprojectbackend.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.example.blogprojectbackend.domain.User;
 import com.example.blogprojectbackend.dto.AddUserRequest;
 import com.example.blogprojectbackend.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public Long save(AddUserRequest dto) {
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(passwordEncoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
